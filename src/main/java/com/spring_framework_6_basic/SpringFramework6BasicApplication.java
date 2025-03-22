15649292249b8a8e8b8f93776ac01e518f372ef4
jorge.uribe <juribe2897@gmail.com>
2025-03-22 10:57:38 -0500

    First commit

---
+++ src/main/java/com/spring_framework_6_basic/SpringFramework6BasicApplication.java
@@ -0,0 +1,13 @@
+package com.spring_framework_6_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework6BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramework6BasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_framework_6_basic/SpringFramework6BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_framework_6_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework6BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
